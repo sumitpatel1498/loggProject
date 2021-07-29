0d06e155f01e9fe2774504efe474904217e3c829
Akash Meena <akash.meena@workpulse.com>
2021-07-29 19:08:41 +0530

    Entity FrameWork Code

---
+++ loggProject/Controllers/UserInfoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace loggProject.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        public ActionResult Index()
+        {
+            DemoDBEntities5 db = new DemoDBEntities5();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Create(UserInfo userinfo)
+        {   
+            if(ModelState.IsValid) {
+                db.UserInfo.Add(userinfo);
+                db.saveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userinfo);
+        }
+    }
+}
